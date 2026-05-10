590bcbae0c86ab1f643ee936cee8bf80ba4e7d36
kcc <jay960323@gmail.com>
2026-05-10 13:58:39 +0800

    新增dinic.cpp

--- codes/Flow/dinic.cpp
+++ codes/Flow/dinic.cpp
@@ -1,64 +1,56 @@
-template<typename T>
-struct DINIC{
-	static const int MAXN=105;
-	static const T INF=INT_MAX;
-	int n, LV[MAXN], cur[MAXN];
-	struct edge{
-		int v,pre;
-		T cap,r;
-		edge(int v,int pre,T cap):v(v),pre(pre),cap(cap),r(cap){}
-	};
-	int g[MAXN];
-	vector<edge> e;
-	void init(int _n){
-		memset(g,-1,sizeof(int)*((n=_n)+1));
-		e.clear();
-	}
-	void add_edge(int u,int v,T cap,bool directed=false){
-		e.push_back(edge(v,g[u],cap));
-		g[u]=e.size()-1;
-		e.push_back(edge(u,g[v],directed?0:cap));
-		g[v]=e.size()-1;
-	}
-	int bfs(int s,int t){
-		memset(LV,0,sizeof(int)*(n+1));
-		memcpy(cur,g,sizeof(int)*(n+1));
-		queue<int> q;
-		q.push(s);
-		LV[s]=1;
-		while(q.size()){
-			int u=q.front();q.pop();
-			for(int i=g[u];~i;i=e[i].pre){
-				if(!LV[e[i].v]&&e[i].r){
-					LV[e[i].v]=LV[u]+1;
-					q.push(e[i].v);
-					if(e[i].v==t)return 1;
-				}
-			}
-		}
-		return 0;
-	}
-	T dfs(int u,int t,T CF=INF){
-		if(u==t||!CF)return CF;
-		T df,tf=0;
-		for(int &i=cur[u];~i;i=e[i].pre){
-			if(LV[e[i].v]==LV[u]+1&&e[i].r){
-				if(df=dfs(e[i].v,t,min(CF,e[i].r))){
-					e[i].r-=df;
-					e[i^1].r+=df;
-					tf+=df;
-					if(!(CF-=df))break;
-				}
-			}
-		}
-		if(!tf)LV[u]=0;
-		return tf;
-	}
-	T dinic(int s,int t,bool clean=true){
-		if(clean)for(size_t i=0;i<e.size();++i)
-			e[i].r=e[i].cap;
-		T ans=0, f=0;
-		while(bfs(s,t))while(f=dfs(s,t))ans+=f;
-		return ans;
-	}
+struct Dinic{
+    struct Edge{
+        int to, cap, rev;
+    };
+    vector<Edge> adj[505];
+    vector<int> level, ptr;
+    int n;
+    Dinic(int _n){
+        n = _n;
+    }
+    void addedge(int u, int v, int w){
+        adj[u].pb({v, w, adj[v].size()});
+        adj[v].pb({u, 0, adj[u].size() - 1});
+    }
+    bool bfs(int s, int t)
+    {
+        fill(level.begin(), level.end(), -1);
+        level[s] = 0;
+        queue<int> q;
+        q.push(s);
+        while(q.size()){
+            int x = q.front(); q.pop();
+            for(auto edge : adj[x]){
+                if(edge.cap > 0 and level[edge.to] == -1){
+                    level[edge.to] = level[x] + 1;
+                    q.push(edge.to);
+                }
+            }
+        }
+        return (level[t] != -1);
+    }
+    int dfs(int now, int t, int pushed)
+    {
+        if(now == t) return pushed;
+        for(int &i = ptr[now]; i < adj[now].size(); i++){
+            auto &w = adj[now][i];
+            if(level[now] + 1 != level[w.to] or w.cap == 0) continue;
+            int tr = dfs(w.to, t, min(pushed, w.cap));
+            if(tr == 0) continue;
+            w.cap -= tr;
+            adj[w.to][w.rev].cap += tr;
+            return tr;
+        }
+        return 0;
+    }
+    int compute_max_flow(){
+        int max_flow = 0;
+        while(bfs(1, n)){
+            fill(ptr.begin(), ptr.end(), 0);
+            while(int pushed = dfs(1, n, 1e18)){
+                max_flow += pushed;
+            }
+        }
+        return max_flow;
+    }
 };
