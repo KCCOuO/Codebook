97f7479b88bdc6d2b93aeede3529183b50ff1814
kcc <jay960323@gmail.com>
2026-06-24 22:48:43 +0800

    新增區間第k小

--- codes/Data Structure/persistent-segment-tree.cpp
+++ codes/Data Structure/persistent-segment-tree.cpp
@@ -23,6 +23,13 @@ int query(int node, int l, int r, int ql, int qr)
 int get_kth(int node_l, int node_r, int l, int r, int k)
 {
     // 區間第k小
+    /*
+    if(l == r) return l;
+    int mid = (l + r) >> 1;
+    int left_cnt = tree[tree[node_r].l].sum - tree[tree[node_l].l].sum;
+    if(k <= left_cnt) return get_kth(tree[node_l].l, tree[node_r].l, l, mid, k);
+    else return get_kth(tree[node_l].r, tree[node_r].r, mid + 1, r, k - left_cnt);
+    */
     while (l < r) {
         int mid = (l + r) >> 1;
         int left_cnt = tree[tree[node_r].l].sum - tree[tree[node_l].l].sum;
