3d0bfd09bae7ba93be35d6bcfd5f1a12bcfd5ca1
kcc <jay960323@gmail.com>
2026-07-08 20:03:29 +0800

    dsu-rollback

--- codes/Data Structure/DSU-rollback.cpp
+++ codes/Data Structure/DSU-rollback.cpp
@@ -5,24 +5,20 @@ struct DSU_rollback{
         int u, v, pre_sz;
     };
     vector<modify> history;
-
     void initial(int n){
         parent.resize(n + 1);
         sz.resize(n + 1, 1);
         components = n;
         for(int i = 1; i <= n; i++) parent[i] = i;
     }
-
     int find(int x){
         while(x != parent[x]) x = parent[x];
         return x;
     }
-
     void unite(int u, int v){
         int root_u = find(u), root_v = find(v);
         if(root_u == root_v) return;
         if(sz[root_u] < sz[root_v]) swap(root_u, root_v);
-        
         history.pb({root_u, root_v, sz[root_u]});
         parent[root_v] = root_u;
         sz[root_u] += sz[root_v];
@@ -32,7 +28,6 @@ struct DSU_rollback{
         if(history.size() == 0) return;
         modify last = history.back();
         history.pop_back();
-
         parent[last.v] = last.v;
         sz[last.u] = last.pre_sz;
         components++;
