84771b36082967dba7c483bce44aa940c7422ebf
kcc <jay960323@gmail.com>
2026-08-12 22:24:45 +0800

    修改convex-hull.cpp

--- codes/Computational Geometry/convex-hull.cpp
+++ codes/Computational Geometry/convex-hull.cpp
@@ -1,30 +1,14 @@
-struct node{
-    int x, y;
-    node operator - (const node &b) {
-        return {x - b.x, y - b.y};
-    }
-    int operator * (const node &b) {
-        return x * b.y - y * b.x;
-    }
-    bool operator < (const node &b) {
-        if (x != b.x) return x < b.x;
-        return y < b.y;
-    }
-};
-vector<node> build(vector<node> v)
+double cross(point a, point b) {return a.x * b.y - a.y * b.x;}
+vector<point> build(vector<point> v)
 {
-    vector<node> ans;
+    vector<point> ans;
     sort(v.begin(), v.end());
     for(int i = 0; i < v.size(); i++){
-        while(ans.size() > 1 and (ans.end()[-2] - ans.end()[-1]) * (ans.end()[-2] - v[i]) > 0){
-            ans.pop_back();
-        }
+        while(ans.size() > 1 and cross(ans.end()[-2] - ans.end()[-1], ans.end()[-2] - v[i]) > 0) ans.pop_back();
         ans.pb(v[i]);
     }
     for(int i = v.size() - 2; i >= 0; i--){
-        while(ans.size() > 1 and (ans.end()[-2] - ans.end()[-1]) * (ans.end()[-2] - v[i]) > 0){
-            ans.pop_back();
-        }
+        while(ans.size() > 1 and cross(ans.end()[-2] - ans.end()[-1], ans.end()[-2] - v[i]) > 0) ans.pop_back();
         ans.pb(v[i]);
     }
     ans.pop_back();
