981dee2943de421dfda56c13b8a93d29548ea7b5
YuutoHoshino <skybluetw1108@gmail.com>
2026-09-17 23:38:23 +0800

    Add files via upload

---
+++ codes/Tarjan/EBCC.cpp
@@ -0,0 +1,66 @@
+// 2.8 EBCC [12a170]  0-indexed, bel[] = 所屬邊雙編號
+struct EBCC { // CF/contest/1986/pF
+    int n, cur, cnt;
+    vector<vector<int>> adj;
+    vector<int> stk, dfn, low, bel;
+    vector<pair<int, int>> bridges; // 關鍵邊
+    EBCC(int n) : n(n), cur
+        (0), cnt(0), adj(n), low(n), dfn(n, -1), bel(n, -1) {}
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    void dfs(int x, int p) {
+        dfn[x] = low[x] = cur++;
+        stk.push_back(x);
+        for (auto y : adj[x]) {
+            if (y == p) continue;
+            if (dfn[y] == -1) {
+                dfs(y, x);
+                low[x] = min(low[x], low[y]);
+                if (low[y] > dfn[x]) {
+                    bridges.emplace_back(x, y);
+                }
+            } else if (bel[y] == -1) {
+                low[x] = min(low[x], dfn[y]);
+            }
+        }
+        if (dfn[x] == low[x]) {
+            int y;
+            do {
+                y = stk.back();
+                bel[y] = cnt;
+                stk.pop_back();
+            } while (y != x);
+            cnt++;
+        }
+    }
+    vector<int> work() { // not connected
+        for (int i = 0; i < n; i++)
+            if (dfn[i] == -1) dfs(i, -1);
+        return bel;
+    }
+    struct Graph {
+        int n;
+        vector<pair<int, int>> edges;
+        vector<int> siz, cnte;
+    };
+    Graph compress() {
+        Graph g;
+        g.n = cnt;
+        g.siz.resize(cnt);
+        g.cnte.resize(cnt);
+        for (int i = 0; i < n; i++) {
+            g.siz[bel[i]]++;
+            for (auto j : adj[i]) {
+                if (bel[i] < bel[j]) {
+                    // ↓ 以下照片被截斷，為補寫
+                    g.edges.emplace_back(bel[i], bel[j]);
+                } else if (bel[i] == bel[j] && i < j) {
+                    g.cnte[bel[i]]++;
+                }
+            }
+        }
+        return g;
+    }
+};
